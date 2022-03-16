61cfdc6f3ed60bcad3e639427065fa119fa66e3b
Guilherme Raposo <gui.raposo36@gmail.com>
2022-03-16 13:52:44 -0300

    feat: finish section

--- src/views/Consortium/index.js
+++ src/views/Consortium/index.js
@@ -1,11 +1,19 @@
 import UpBar from "../../components/UpBar";
-import "./styles.scss"
+import "./styles.scss";
 
 export default function Consortium() {
   return (
-    <div>
+    <div className="container">
       <UpBar />
-      <h1>Hello World</h1>
+      <section>
+        <div className="content">
+        <h1>Consórcio do seu jeito!</h1>
+        <p>
+          Consórcio do seu jeito! Planejar seu futuro com o investimento certo,
+          que melhor se encaixa no seu perfil e com toda a segurança!
+        </p>
+        </div>
+      </section>
     </div>
   );
 }
