96d4872dce1ad8588f03f1d256e79410f4458a7a
Guilherme Raposo <gui.raposo36@gmail.com>
2022-03-17 09:58:34 -0300

    refactor: applied responsiveness in the upbar

--- src/components/UpBar/index.js
+++ src/components/UpBar/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./styles.scss";
 import Logo from "../../assets/Images/logo.png";
+import LogoMin from "../../assets/Images/logoMin.svg";
 import TCS from "../../assets/Images/tcs.png";
 
 export default function UpBar() {
@@ -13,13 +14,13 @@ export default function UpBar() {
     { cursor: "pointer" },
     { cursor: "pointer" },
     { cursor: "pointer" },
-    { cursor: "pointer" }
+    { cursor: "pointer" },
   ]);
 
-  useEffect(()=>{
+  useEffect(() => {
     navigate(``, 6);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   function navigateTo(url, pos) {
     navigate(`/${url}`);
@@ -35,33 +36,69 @@ export default function UpBar() {
   }
 
   return (
-    <nav>
-      <img className="logo" src={Logo} alt="logo-vivo" />
-      <div style={styles[0]} className="link" onClick={() => navigateTo("Auto", 0)}>
-        Seguro Auto
-      </div>
-      <div style={styles[1]} className="link" onClick={() => navigateTo("Pet", 1)}>
-        Seguro Pet
-      </div>
-      <div style={styles[2]} className="link" onClick={() => navigateTo("Residential", 2)}>
-        Seguro Residencial
-      </div>
-      <div style={styles[3]} className="link" onClick={() => navigateTo("Travel", 3)}>
-        Seguro Viagem
-      </div>
-      <div style={styles[4]} className="link" onClick={() => navigateTo("Life", 4)}>
-        Seguro Vida
-      </div>
-      <div style={styles[5]} className="link" onClick={() => navigateTo("Bail", 5)}>
-        Seguro Fiança
-      </div>
-      <div style={styles[6]} className="link" onClick={() => navigateTo("", 6)}>
-        Consórcio
-      </div>
-      <div className="tcs">
-        <div>Made by:</div>
+    <div>
+      <div className="menu">
+        <img className="logo" src={LogoMin} alt="logo-vivo" />
         <img className="logo-tcs" src={TCS} alt="logo-tcs" />
+        <i class="bx bx-menu"></i>
       </div>
-    </nav>
+
+      <nav>
+        <img className="logo" src={Logo} alt="logo-vivo" />
+        <div
+          style={styles[0]}
+          className="link"
+          onClick={() => navigateTo("Auto", 0)}
+        >
+          Seguro Auto
+        </div>
+        <div
+          style={styles[1]}
+          className="link"
+          onClick={() => navigateTo("Pet", 1)}
+        >
+          Seguro Pet
+        </div>
+        <div
+          style={styles[2]}
+          className="link"
+          onClick={() => navigateTo("Residential", 2)}
+        >
+          Seguro Residencial
+        </div>
+        <div
+          style={styles[3]}
+          className="link"
+          onClick={() => navigateTo("Travel", 3)}
+        >
+          Seguro Viagem
+        </div>
+        <div
+          style={styles[4]}
+          className="link"
+          onClick={() => navigateTo("Life", 4)}
+        >
+          Seguro Vida
+        </div>
+        <div
+          style={styles[5]}
+          className="link"
+          onClick={() => navigateTo("Bail", 5)}
+        >
+          Seguro Fiança
+        </div>
+        <div
+          style={styles[6]}
+          className="link"
+          onClick={() => navigateTo("", 6)}
+        >
+          Consórcio
+        </div>
+        <div className="tcs">
+          <div>Made by:</div>
+          <img className="logo-tcs" src={TCS} alt="logo-tcs" />
+        </div>
+      </nav>
+    </div>
   );
 }
